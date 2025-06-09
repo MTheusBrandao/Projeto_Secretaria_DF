1a774e5eebdc6c0a0cc932318c7bb00e0ab920de
Matheus Dias <matheusunai@gmail.com>
2025-06-09 19:46:12 -0300

    feat: atualizao no init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,4 +10,7 @@ def criar_aplicacao(config_class=Config):
     db.init.app(app)
     jwt.init_app(app)
 
+    from .routes.autenticacao_service import bp as autenticacao_bp
+    app.register_blueprint(autenticacao_bp)
+
     return app
