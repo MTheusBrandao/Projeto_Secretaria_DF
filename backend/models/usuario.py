4904d4df98f3ea68281937fd81ce462b486e30aa
Matheus Dias <matheusunai@gmail.com>
2025-06-09 19:00:02 -0300

    feat: criação do model usuario

---
+++ backend/models/usuario.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from ..extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha_hash = db.Column(db.String(128), nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=True)
+    telefone = db.Column(db.String(15), nullable=True)
+    endereco = db.Column(db.Text, nullable=True)
+    tipo = db.Column(db.String(20), nullable=False, default='paciente')
+    criado_em = db.Column(db.DateTime, default=datetime.now())
+    ativo = db.Column(db.Boolean, default=True)
+
+    def set_senha(self, senha):
+        self.senha_hash = generate_password_hash(senha)
+
+    def check_senha(self, senha):
+        return check_password_hash(self.senha_hash, senha)
+    
+    def __repr__(self):
+        return f'<Usuario {self.email}>'
