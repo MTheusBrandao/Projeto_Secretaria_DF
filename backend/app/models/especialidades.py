49030692dba37cecc930fd658b0ac9fbf872095b
Matheus Dias <matheusunai@gmail.com>
2025-06-10 01:27:01 -0300

    feat: criação do modelo especialidades

---
+++ backend/app/models/especialidades.py
@@ -0,0 +1,18 @@
+from ..extensions import db
+
+class Especialidade(db.Model):
+    __tablename__: 'especialidades'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    codigo = db.Column(db.String(10), unique=True, nullable=False)
+    descricao = db.Column(db.Text, nullable=True)
+    ativa = db.Column(db.Boolean, default=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'codigo': self.codigo,
+            'descricao': self.descricao
+        }
